1931256d1b65cf5069fabce2ebee548e4164e601
Dubabbi <thdms3160@naver.com>
2024-09-05 16:36:15 +0900

    Feat: chat 버튼 수정

--- src/components/ChatButton/ChatButton.jsx
+++ src/components/ChatButton/ChatButton.jsx
@@ -8,9 +8,9 @@ export default function ChatButton() {
         <a href ="">
             <img
                 src="https://vendor-cdn.imweb.me/images/kakao-talk-button-default.svg" alt="카카오톡 채널 채팅하기 버튼"
-                width="70vw" 
-                height="70vw" 
-                style={{position: 'fixed', right: 25, bottom: 25, cursor: 'pointer'}}/>
+                width="90vw" 
+                height="90vw" 
+                style={{position: 'fixed', right: 30, bottom: 15, cursor: 'pointer'}}/>
         </a>
     );
 }
