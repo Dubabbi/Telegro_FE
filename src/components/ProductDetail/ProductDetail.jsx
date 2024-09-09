478a73dea8a05f7775458c4f2fa0a2c98feb904a
Dubabbi <thdms3160@naver.com>
2024-09-09 17:01:50 +0900

    Design: 제품 상세 페이지 디자인 완성

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,59 +1,70 @@
 import React from 'react';
 import styled from 'styled-components';
-import img from '../Check/image.svg';
-
+import img from '../Check/image.svg'; 
 const ProductPageWrapper = styled.div`
   display: flex;
   justify-content: center;
-  margin: 50px;
+  margin-left: 15%;
+  margin-top: 2%;
+  margin-bottom: 4%;
   padding: 20px;
-  border-radius: 10px;
-  background-color: #f5f5f5;
+  background-color: #fff;
+  border: 1px solid #d3d3d3;
+  border-radius: 15px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  width: 70%;
+  flex-direction: column;
 `;
 
 const ProductDetails = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 80%;
-  background-color: #ffffff;
-  border: 1px solid #d3d3d3;
-  border-radius: 10px;
+  width: 100%;
   padding: 20px;
+  border-radius: 10px;
+  margin-bottom: 40px;
 `;
 
-const ProductInfo = styled.div`
-  flex: 2;
-  display: flex;
-  flex-direction: column;
+const ProductInfoWrapper = styled.div`
+  display: flex; /* 가로로 배치 */
   align-items: center;
-  padding-right: 20px;
+  flex: 2;
 `;
 
 const ProductImage = styled.img`
-  width: 300px;
+  width: 250px;
   height: auto;
-  border-radius: 10px;
-  margin-bottom: 20px;
+  margin-right: 30px;
+`;
+
+const ProductInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 `;
 
 const ProductTitle = styled.h2`
-  font-size: 1.5rem;
-  color: #003087;
+  font-weight: bold;
+  font-size: 2rem;
+  color: #303972;
   margin-bottom: 10px;
+  margin-left: 6%;
 `;
 
 const ProductSubtitle = styled.h3`
-  font-size: 1.2rem;
-  color: #333;
-  margin-bottom: 20px;
+  font-weight: bold;
+  font-size: 1.6rem;
+  color: #6B6B6B;
+  margin-bottom: 35%;
+  margin-left: 8%;
 `;
 
 const BuyButton = styled.button`
-  padding: 10px 20px;
-  background-color: #0f62fe;
+  padding: 10px 25px;
+  background-color: #4D44B5;
   color: white;
   font-size: 1rem;
-  border-radius: 5px;
+  border-radius: 1.5rem;
   border: none;
   cursor: pointer;
   margin-bottom: 20px;
@@ -61,49 +72,75 @@ const BuyButton = styled.button`
 
 const PriceWrapper = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin-left: 5%;
   margin-bottom: 20px;
+  h1{
+    font-size: 1.4rem;
+  }
 `;
 
 const PriceTag = styled.span`
   padding: 5px 15px;
-  background-color: #e0e0e0;
-  border-radius: 20px;
-  color: #555;
-  font-size: 1rem;
+  background: rgba(77, 68, 181, 0.2);
+  border-radius: 1rem;
+  align-items: center;
+  color: #444;
+  font-weight: semibold;
+  font-size: 1.3rem;
 `;
 
 const AdditionalImagesWrapper = styled.div`
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  border-left: 1px solid #d3d3d3;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  row-gap: 5px;  /* 위아래 간격을 5px로 설정 */
+  column-gap: 10px;  /* 좌우 간격 */
   padding-left: 20px;
+  align-items: center;
+  justify-items: center;
+  height: 29.5vh;
+  margin: 5% 0;
 `;
 
 const AdditionalImage = styled.img`
   width: 80px;
   height: auto;
   border-radius: 5px;
-  margin-bottom: 10px;
+  border: 1px solid #d3d3d3;
+  `;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: row; /* 좌우로 배치 */
+  justify-content: space-between; /* 좌우 정렬 */
+  width: 90%;
+  margin-left: 5%;
+  margin-top: 20px;
+  padding-bottom: 5%;
+  padding-top: 3%;
 `;
 
 const DescriptionWrapper = styled.div`
-  margin-top: 30px;
-  width: 100%;
+  flex: 1;
+  margin-right: 20px;
 `;
 
 const DescriptionTitle = styled.h4`
-  font-size: 1.2rem;
-  color: #003087;
+  font-size: 1.6rem;
+  color: #303972;
+  font-weight: bold;
   margin-bottom: 10px;
 `;
 
 const DescriptionList = styled.ul`
   list-style-type: none;
+  margin-top: 2%;
   padding: 0;
-  color: #555;
+  font-size: 1.4rem;
+  line-height: 1.7;
+  color: #444;
 `;
 
 const DescriptionItem = styled.li`
@@ -111,13 +148,13 @@ const DescriptionItem = styled.li`
 `;
 
 const RequestInputWrapper = styled.div`
-  margin-top: 30px;
-  width: 100%;
+  flex: 1;
 `;
 
 const RequestInputLabel = styled.label`
-  font-size: 1.2rem;
-  color: #003087;
+  font-size: 1.6rem;
+  color: #444;
+  font-weight: bold;
   margin-bottom: 10px;
   display: block;
 `;
@@ -126,56 +163,83 @@ const RequestInput = styled.textarea`
   width: 100%;
   height: 100px;
   padding: 10px;
-  border: 1px solid #d3d3d3;
+  border: 1px solid #777;
   border-radius: 10px;
   resize: none;
+    &::placeholder {
+      color: #444;
+    }
+    &:focus {
+      outline: 2px solid #94A3D8;
+      border: none;
+    }
+`;
+
+const Title = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 70%;
+  margin-top: 5%;
+  margin-left: 15%;
+  justify-content: space-between;
+  align-items: center;
+  h1 {
+    font-size: 1.7rem;
+    font-weight: bold;
+  }
+  
+
 `;
 
 const ProductDetail = () => {
   return (
     <>
-    <div style={{width: '100%', minHeight: '22.6vh', border: 'none'}}></div>
-    <ProductPageWrapper>
-      <ProductDetails>
-        {/* 좌측 상품 정보 */}
-        <ProductInfo>
-          <ProductImage src={img} alt="Main Product" />
-          <ProductTitle>상품명</ProductTitle>
-          <ProductSubtitle>제품명</ProductSubtitle>
-          <BuyButton>구매하기</BuyButton>
-          <PriceWrapper>
-            <PriceTag>Biz: ₩160,000</PriceTag>
-            <PriceTag>B: ₩110,000</PriceTag>
-            <PriceTag>D: ₩100,000</PriceTag>
-            <PriceTag>C: ₩90,000</PriceTag>
+      <div style={{ width: '100%', minHeight: '22.6vh', border: 'none' }}></div>
+      <Title><h1>제품 상세</h1></Title>
+      <ProductPageWrapper>
+        <ProductDetails>
+          {/* 상품 이미지와 정보가 가로로 정렬된 섹션 */}
+          <ProductInfoWrapper>
+            <ProductImage src={img} alt="Main Product" />
+            <ProductInfo>
+              <ProductTitle>상품명</ProductTitle>
+              <ProductSubtitle>제품명</ProductSubtitle>
+              <BuyButton>구매하기</BuyButton>
+            </ProductInfo>
+          </ProductInfoWrapper>
+          {/* 우측 추가 상품 이미지 */}
+          <AdditionalImagesWrapper>
+            <AdditionalImage src={img} alt="Additional Image 1" />
+            <AdditionalImage src={img} alt="Additional Image 2" />
+            <AdditionalImage src={img} alt="Additional Image 3" />
+            <AdditionalImage src={img} alt="Additional Image 4" />
+          </AdditionalImagesWrapper>
+        </ProductDetails>
+        <PriceWrapper>
+          <DescriptionTitle style={{marginBottom: '0px', alignItems: 'center'}}>가격</DescriptionTitle>
+          <PriceTag>Biz: ₩160,000</PriceTag>
+          <PriceTag>B: ₩110,000</PriceTag>
+          <PriceTag>D: ₩100,000</PriceTag>
+          <PriceTag>C: ₩90,000</PriceTag>
           </PriceWrapper>
-        </ProductInfo>
+        <ContentWrapper>
+          <DescriptionWrapper>
+            <DescriptionTitle>상품 설명</DescriptionTitle>
+            <DescriptionList>
+              <DescriptionItem>- 이 상품은 이런 특징이 있어요.</DescriptionItem>
+              <DescriptionItem>- ~~~~한 장점도 있어요.</DescriptionItem>
+              <DescriptionItem>- 다른 상품들과 이런 차이점이 있어요.</DescriptionItem>
+              <DescriptionItem>- 이 상품은 다양한 장점이 있어요.</DescriptionItem>
+              <DescriptionItem>- 만족도가 높은 제품이에요.</DescriptionItem>
+            </DescriptionList>
+          </DescriptionWrapper>
 
-        {/* 우측 추가 상품 이미지 */}
-        <AdditionalImagesWrapper>
-          <AdditionalImage src={img} alt="Additional Image 1" />
-          <AdditionalImage src={img} alt="Additional Image 2" />
-          <AdditionalImage src={img} alt="Additional Image 3" />
-          <AdditionalImage src={img} alt="Additional Image 4" />
-        </AdditionalImagesWrapper>
-
-        {/* 상품 설명과 요청사항 입력란 */}
-        <DescriptionWrapper>
-          <DescriptionTitle>상품 설명</DescriptionTitle>
-          <DescriptionList>
-            <DescriptionItem>이 상품은 이런 특징이 있어요.</DescriptionItem>
-            <DescriptionItem>~~~~한 장점도 있어요.</DescriptionItem>
-            <DescriptionItem>다른 상품들과 이런 차이점이 있어요.</DescriptionItem>
-            <DescriptionItem>이 상품은 다양한 장점이 있어요.</DescriptionItem>
-            <DescriptionItem>만족도가 높은 제품이에요.</DescriptionItem>
-          </DescriptionList>
           <RequestInputWrapper>
             <RequestInputLabel>요청 사항</RequestInputLabel>
             <RequestInput placeholder="요청 사항을 입력해 주세요." />
           </RequestInputWrapper>
-        </DescriptionWrapper>
-      </ProductDetails>
-    </ProductPageWrapper>
+        </ContentWrapper>
+      </ProductPageWrapper>
     </>
   );
 };
