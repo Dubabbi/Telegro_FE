e0d33459ff1d7b667f1a5cbc4e8bf12c97ba940d
Dubabbi <thdms3160@naver.com>
2024-09-09 11:44:42 +0900

    Design: Landing 페이지 완성

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -5,9 +5,12 @@ import Form from 'react-bootstrap/Form';
 import Img from '/src/assets/image/Landing/headset.svg';
 import Next from '/src/assets/image/Landing/next.svg';
 import NextBlue from '/src/assets/image/Landing/nextBlue.svg';
+import Logo from '/src/assets/image/Landing/logo.svg';
+import Mail from '/src/assets/image/Landing/mail.svg';
 
 export default function Landing() {
   return (
+    <>
     <L.LandingWrapper>
     <L.NavWrapper>
       <L.Logo href="/main">Telegro</L.Logo>
@@ -24,6 +27,45 @@ export default function Landing() {
       </L.Content>
       <L.Img src={Img} />
       </L.Inline>
+      <L.Intro>
+        <L.InlineIntro>
+          <L.Title>Telegro</L.Title>
+          <L.About>
+            <p>기술개발 전문제조 공급 유통</p>
+            <h1>헤드셋 녹취 장비 전문</h1>
+          </L.About>
+        </L.InlineIntro>
+      </L.Intro>
+      <L.List>
+        <p>헤드셋</p><p>전화/증폭기</p><p>라인코드</p><p>녹음기기</p><p>악세서리</p>
+      </L.List>
+      <L.FooterWrapper>
+        <L.FooterInline>
+          <L.FooterTitle>
+            <img src={Logo} />
+            <p>Telegro</p>
+          </L.FooterTitle>
+          <L.FooterBox>
+          <L.FooterEnd>
+            <img src={Mail} />
+            <p>Telegro@telegro.com</p>
+          </L.FooterEnd>
+          <L.Contact>Contact Us</L.Contact>
+          </L.FooterBox>
+        </L.FooterInline>
+        <hr style={{margin: '2%', width: '90%', marginLeft: '5%', color: '#C1C7CD'}}/>
+        <L.TextWrapper>
+          <h1>회사명: 서연전자</h1>
+          <p>주소: 서울특별시 광진구 광나루로56길 85 테크노마트 21 8층 A30, 31호</p>
+          <p>고객센터: 070-4240-7422</p>
+          <p>대표이사: 연경진</p>
+          <p>사업자 등록번호: 215-18-12286</p>
+        </L.TextWrapper>
+        <hr style={{margin: '2%', width: '90%', marginLeft: '5%', color: '#C1C7CD'}}/>
+        <L.Copyright>Telegro @ 2024. All rights reserved.</L.Copyright>
+      </L.FooterWrapper>
     </L.LandingWrapper>
+
+    </>
   );
 }
--- src/components/Landing/LandingStyle.jsx
+++ src/components/Landing/LandingStyle.jsx
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 export const NavWrapper = styled.div`
   top: 0;
   width: 100%;
@@ -27,9 +25,14 @@ export const Inline = styled.div`
     justify-content: space-around;
     align-items: center;
     margin: 10%;
+
+    margin-bottom: 5%;
+    flex-wrap: wrap;
 `
 
 export const Content = styled.div`
+    position: relative;
+    flex: 1;
     h1{
         font-size: 3rem;
         font-weight: bold;
@@ -42,7 +45,7 @@ export const Content = styled.div`
 `
 
 export const Img = styled.img`
-    max-width: 50%;
+    max-width: 40%;
     height: auto;
 `
 
@@ -101,4 +104,163 @@ export const SecondButton = styled.button`
         margin: 1%;
         align-items: center;
     }
+`
+
+export const Intro = styled.div`
+    text-align: center;
+    align-items: center;
+    position: relative;
+    justify-content: center;
+    margin-bottom: 5%;
+`
+
+export const InlineIntro = styled.div`
+    text-align: center;
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: 3%;
+`
+
+export const Title = styled.div`
+    background-color: #0F62FE;
+    color: #fff;
+    font-size: 2.2rem;
+    padding: 0.8% 2%;
+    border-radius: 3rem;
+    font-weight: bold;
+`
+
+export const About = styled.div`
+    font-size: 1rem;
+    p{
+        font-size: 1.3rem;
+        font-weight: bold;
+        margin-bottom: 1%;
+    }
+
+    h1{
+        color: #0F62FE;
+        font-weight: bold;
+        font-size: 2rem;
+    }
+`
+
+export const List = styled.div`
+    font-size: 1.3rem;
+    font-weight: bold;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    margin: 2%;
+`
+
+export const FooterWrapper = styled.div`
+    background-color: #697077;
+    font-size: 1.5rem;
+    padding: 2% 0;
+    margin-top: 4%;
+`
+
+export const FooterInline = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 80%;
+    margin-left: 10%;
+
+`
+
+export const FooterTitle = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 5%;
+
+    img{
+        max-width: 30%;
+    }
+    p{
+        font-size: 1.7rem;
+        color: #C1C7CD;
+        font-weight: bold;
+    }
+
+`
+export const FooterEnd = styled.div`
+    display: flex;
+    background-color: #F2F4F8;
+    border-radius: 2px;
+    padding-left: 1%;
+    padding-right: 3%;
+    align-items: center;
+    img{
+        max-width: 20%;
+        align-items: center;
+    }
+    p{
+        font-size: 1.2rem;
+        color: #697077;
+        align-items: center;
+        margin-bottom: 3%;
+    }
+
+`
+
+export const FooterBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    white-space: nowrap;
+    width: 40%;
+    gap: 2%;
+`
+
+export const Contact = styled.button`
+    background-color: #0F62FE;
+    color: #fff;
+    border: 1px solid #0F62FE;
+    border-radius: 0.2rem;
+    font-size: 1.5rem;
+    padding: 2% 3%;
+    border-radius: 2px;
+    align-items: center;
+    display: flex;
+    font-weight: Semibold;
+    flex-direction: row;
+    justify-content: space-around;
+    p{
+        align-items: center;
+        margin: 1%;
+    }
+`
+export const TextWrapper = styled.div`
+    flex-direction: column;
+    text-align: left;
+    margin-top: 3%;
+    margin-bottom: 3%;
+    color: #fff;
+    margin-left: 5%;
+    h1{
+        font-size: 1.4rem;
+        font-weight: semibold;
+        margin-bottom: 1.3%;
+    }
+
+    p{
+        font-size: 1.2rem;
+        margin-bottom: 0.7%;
+    }
+`
+
+export const Copyright = styled.div`
+    flex-direction: column;
+    text-align: left;
+    margin-top: 2%;
+    margin-bottom: 2%;
+    color: #fff;
+    margin-left: 5%;
+    font-size: 1.3rem;
 `
