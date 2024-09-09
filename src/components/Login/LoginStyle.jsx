2abdf944814009a7d7b9a9db71fc03ab8936ebb9
Dubabbi <thdms3160@naver.com>
2024-09-09 19:33:08 +0900

    Design: 로그인 디자인 수정

--- src/components/Login/LoginStyle.jsx
+++ src/components/Login/LoginStyle.jsx
@@ -19,7 +19,7 @@ export const LoginSection = styled.section`
   height: auto;
   box-shadow: 0 0.4rem 0.8rem rgba(0, 0, 0, 0.1);
   background: #fff;
-  padding: 50px 50px;
+  padding: 5% 6%;
   border-radius: 10px;
   position: relative;
   border-radius: 1rem;
