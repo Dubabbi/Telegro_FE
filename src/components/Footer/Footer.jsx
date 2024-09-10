78f1db7ec09c66e896413ef6340a35c561d25305
Dubabbi <thdms3160@naver.com>
2024-09-10 21:59:46 +0900

    Feat: Footer 수정

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -23,11 +23,12 @@ export default function Footer() {
         </L.Footerline>
         <hr style={{margin: '2%', width: '90%', marginLeft: '5%', color: '#C1C7CD'}}/>
         <L.TextWrapper>
-          <h1>회사명: 서연전자</h1>
+          <h1>회사명: Telegro(서연전자)</h1>
           <p>주소: 서울특별시 광진구 광나루로56길 85 테크노마트 21 8층 A30, 31호</p>
           <p>고객센터: 070-4111-5733</p>
           <p>대표이사: 연경진</p>
           <p>사업자 등록번호: 215-18-12286</p>
+          <p>통신 판매업 번호</p>
         </L.TextWrapper>
         <hr style={{margin: '2%', width: '90%', marginLeft: '5%', color: '#C1C7CD'}}/>
         <L.Copyright>Telegro @ 2024. All rights reserved.</L.Copyright>
