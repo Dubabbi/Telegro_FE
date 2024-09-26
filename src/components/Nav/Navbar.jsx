736c27957c365aa6fd763badaa8a73ff985f4b42
Dubabbi <thdms3160@naver.com>
2024-09-26 13:58:58 +0900

    Feat: Navbar 수정

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import * as N from './NavbarStyle';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaBars, FaTimes } from 'react-icons/fa';
 import Avvvatars from 'avvvatars-react';
 
 export default function Navbar() {
   const [searchValue, setSearchValue] = useState('');
   const [isSearchOpen, setIsSearchOpen] = useState(false); // 검색 창 열림 여부 상태
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // 메뉴 토글 상태
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false); // 서브 메뉴 토글 상태
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,45 +15,65 @@ export default function Navbar() {
     setSearchValue('');
   };
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const toggleSubMenu = () => {
+    setIsSubMenuOpen(!isSubMenuOpen);
+  };
+
   return (
-    <>
-      <N.NavWrapper>
-        {/* Main Navigation */}
-        <N.NavContainer>
-          <N.Logo href="/main">Telegro</N.Logo>
-          <N.MainNav>
-            <li><a href="/main">Home</a></li>
-            <li><a href="/signup">회원가입</a></li>
-            <li><a href="/cart">장바구니</a></li>
-            <li><a href="/login">로그아웃</a></li>
-            <li><a href="mailto:ykjroom@naver.com">Contact Us</a></li>
-          </N.MainNav>
-          {/* Avatar */}
-          <Avvvatars value="user" size={40} style={{ marginLeft: '20px' }} />
-        </N.NavContainer>
+    <N.NavWrapper>
+      <N.NavContainer>
+        <N.Logo href="/main">Telegro</N.Logo>
 
-        <N.SecondaryNavContainer>
-          <N.SecondaryNav>
-            <li><a href="/headset">헤드셋</a></li>
-            <li><a href="/lineCord">라인코드</a></li>
-            <li><a href="/recording">녹음기기</a></li>
-            <li><a href="/accessory">악세서리</a></li>
-            <li><a href="/notice">자료실</a></li>
-          </N.SecondaryNav>
+        {/* 햄버거 메뉴 버튼 (780px 이하에서만 보임) */}
+        <N.HamburgerButton onClick={toggleMenu}>
+          {isMenuOpen ? <FaTimes size={30} /> : <FaBars size={30} />}
+        </N.HamburgerButton>
 
-          <N.SearchWrapper>
-            <N.StyledForm onSubmit={handleSubmit}>
-              <N.SearchInput
-                type="text"
-                placeholder="검색"
-                value={searchValue}
-                onChange={(e) => setSearchValue(e.target.value)}
-              />
-              <N.StyledButton type="submit"><FaSearch size={15} /></N.StyledButton>
-            </N.StyledForm>
-          </N.SearchWrapper>
-        </N.SecondaryNavContainer>
-      </N.NavWrapper>
-    </>
+        {/* Main Navigation (항상 보임) */}
+        <N.MainNav>
+          <li><a href="/main">Home</a></li>
+          <li><a href="/signup">회원가입</a></li>
+          <li><a href="/cart">장바구니</a></li>
+          <li><a href="/login">로그아웃</a></li>
+          <li><a href="mailto:ykjroom@naver.com">Contact Us</a></li>
+        </N.MainNav>
+
+        {/* Avatar (마이페이지 아이콘) */}
+        <Avvvatars value="user" size={40} style={{ marginLeft: '20px' }} />
+      </N.NavContainer>
+
+      {/* 검색창 (항상 보임) */}
+      <N.SearchWrapper>
+        <N.StyledForm onSubmit={handleSubmit}>
+          <N.SearchInput
+            type="text"
+            placeholder="검색"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+          <N.StyledButton type="submit"><FaSearch size={15} /></N.StyledButton>
+        </N.StyledForm>
+      </N.SearchWrapper>
+
+      {/* 토글 메뉴가 열리면 나오는 부분 (780px 이하에서만 보임) */}
+      {isMenuOpen && (
+        <N.NavMenu>
+          <N.SubMenu onClick={toggleSubMenu}>상품 보기</N.SubMenu>
+          {isSubMenuOpen && (
+            <N.SecondaryNav>
+              <li><a href="/headset">헤드셋</a></li>
+              <li><a href="/lineCord">라인코드</a></li>
+              <li><a href="/recording">녹음기기</a></li>
+              <li><a href="/accessory">악세서리</a></li>
+              <li><a href="/notice">자료실</a></li>
+            </N.SecondaryNav>
+          )}
+        </N.NavMenu>
+      )}
+    </N.NavWrapper>
   );
 }
