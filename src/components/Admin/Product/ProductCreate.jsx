322f28405fdc1cb62d74cbcd73c0973cfb8d76a8
Dubabbi <thdms3160@naver.com>
2024-10-12 13:10:36 +0900

    Feat: ProductCreate 수정

--- src/components/Admin/Product/ProductCreate.jsx
+++ src/components/Admin/Product/ProductCreate.jsx
@@ -1,280 +1,293 @@
-import React, { useState, useRef } from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React, { useState, useRef, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import * as D from '../NoticeDetail/NoticeDetailStyle';
 import * as N from '../Notice/NoticeStyle';
 import { Editor } from '@toast-ui/react-editor'; 
 import '@toast-ui/editor/dist/toastui-editor.css'; 
-
-const MainWrapper = styled.div`
-  width: 78%; 
-  margin-left: 22%;
-  padding: 20px;
-`;
-
-
-const FormWrapper = styled.div`
-  width: 100%;
-  border: 1px solid #e5e5e5;
-  border-radius: 8px;
-`;
-
-const SectionTitleWrapper = styled.div`
-  background-color: #f2f2f2; 
-  padding: 1%;
-`;
-
-const SectionTitle = styled.h3`
-  font-size: 2.3rem;
-  font-weight: bold;
-`;
-
-
-const Label = styled.label`
-  font-size: 1.2rem;
-  font-weight: bold;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10%;
-  margin-top: 3%;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  font-size: 1rem;
-  box-sizing: border-box;
-`;
-
-const Select = styled.select`
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10%;
-  border: 1px solid #ddd;
-  margin-top: 3%;
-  border-radius: 5px;
-  font-size: 1rem;
-  box-sizing: border-box;
-`;
-
-const FileInput = styled.input`
-  width: 100%;
-  padding: 10px;
-  margin-bottom: 10%;
-  margin-top: 3%;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  font-size: 1rem;
-  box-sizing: border-box;
-`;
-
-
-const RowContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr); 
-  grid-column-gap: 20px;
-  margin-bottom: 10px;
-`;
-
-
-const NameRowContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr); 
-  grid-column-gap: 20px;
-  margin-bottom: 10px;
-`;
-
-
-const RightColumn = styled.div`
-  flex-direction: row;
-  display: flex;
-  justify-content: space-between;
-`;
-
-
-const SplitContainer = styled.div`
-  display: grid;
-  grid-template-columns: 2fr 1fr; 
-  grid-gap: 20px;
-`;
+import axios from 'axios';
+import * as C from './ProductCreateStyle';
 
 const ProductCreate = () => {
-  const [product, setProduct] = useState({
-    modelName: '',
-    productName: '',
-    option: '',
-    stock: '',
-    category: '',
-    subCategory: '',
-    content: '',
-    businessPrice: '',
-    bestPrice: '',
-    dealerPrice: '',
-    customerPrice: '',
-    photo: ''
-  });
-
+  const navigate = useNavigate();
   const editorRef = useRef();
+  const [product, setProduct] = useState({
+    productModel: '',
+    productName: '',
+    options: [],
+    category: '',
+    content: '',
+    priceBusiness: '',
+    priceBest: '',
+    priceDealer: '',
+    priceCustomer: '',
+    pictures: []
+  });
+  useEffect(() => {
+    if (editorRef.current) {
+      const editorInstance = editorRef.current.getInstance();
+      // Now you can safely use editorInstance
+      console.log(editorInstance.getHTML()); // Example usage
+    }
+  }, []);
 
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    setProduct({ ...product, [name]: value });
+    if (name === 'category') {
+      setProduct(prev => ({
+        ...prev,
+        category: value 
+      }));
+    } else if (name === 'options') {
+      // Assuming options should be stored as an array of strings
+      const optionsArray = value.split(',').map(item => item.trim());
+      setProduct(prev => ({
+          ...prev,
+          options: optionsArray
+      }));
+
+    } else {
+      setProduct(prev => ({
+        ...prev,
+        [name]: value
+      }));
+    }
   };
 
   const handleEditorChange = () => {
-    const data = editorRef.current.getInstance().getHTML(); // 에디터 내용을 HTML 형태로 저장
-    setProduct({ ...product, content: data }); // content 필드에 저장
+    if (editorRef.current) {
+      const htmlContent = editorRef.current.getInstance().getHTML();
+      setProduct(prev => ({ ...prev, content: htmlContent }));
+    } else {
+      console.error('Editor not initialized');
+    }
   };
+
+  const addImageBlobHook = async (blob, callback) => {
+    const formData = new FormData();
+    formData.append('image', blob);
+    
+    const token = localStorage.getItem('token'); // 토큰 가져오기
+    if (!token) {
+      alert('로그인이 필요합니다.');
+      return; 
+    }
+    
+    try {
+      const response = await axios.post(`/proxy/api/file?prefix=product`, formData, {
+        headers: {
+          'Authorization': `Bearer ${token}`, 
+          'Content-Type': 'multipart/form-data' 
+        }
+      });
+    
+      const imageUrl = response.data.data.url;
+      setProduct(prev => ({
+        ...prev,
+        pictures: [...prev.pictures, imageUrl]
+      }));
+    
+      callback(imageUrl, 'Image');
+      
+
+      updateImageMetadata(imageUrl, {
+        description: "새로운 이미지 설명",
+        tags: ["태그1", "태그2"]
+      });
+      
+    } catch (error) {
+      console.error('Image upload failed:', error.response.data.message); 
+      alert('이미지 업로드 실패: ' + error.response.data.message);
+    }
+  };
+  
+  const updateImageMetadata = async (imageUrl, metadata) => {
+    try {
+      const response = await axios.put(`https://thingproxy.freeboard.io/fetch/${imageUrl}`, metadata);
+      console.log('Metadata updated successfully:', response);
+    } catch (error) {
+      console.error('Failed to update metadata:', error.response ? error.response.data : error.message);
+    }
+  };
+  
+
+  const handleCreateProduct = async () => {
+    const formData = {
+      ...product,
+      pictures: product.pictures
+    };
+
+    try {
+      const accessToken = localStorage.getItem('token');
+      const response = await axios.post('/proxy/api/products', formData, {
+        headers: { Authorization: `Bearer ${accessToken}` }
+      });
+      if (response.status === 200) {
+        alert('Product registered successfully.');
+        navigate('/adminproductlist');
+      }
+    } catch (error) {
+      console.error('Error while registering product:', error);
+      alert('Failed to register product');
+    }
+  };
+
   return (
-    <MainWrapper>
-      <SectionTitle style={{margin: '1%', marginBottom: '4%', marginTop: '3%'}}>상품 등록</SectionTitle>
-      <FormWrapper>
-        <SectionTitleWrapper>
-        <SectionTitle style={{fontSize: '1.5vw'}}>상품 등록</SectionTitle>
-        </SectionTitleWrapper>
+    <C.MainWrapper>
+      <C.SectionTitle style={{margin: '1%', marginBottom: '4%', marginTop: '3%'}}>상품 등록</C.SectionTitle>
+      <C.FormWrapper>
+      <C.SectionTitleWrapper>
+        <C.SectionTitle style={{fontSize: '1.5vw'}}>상품 등록</C.SectionTitle>
+        </C.SectionTitleWrapper>
         <div style={{padding: '2%'}}>
         {/* 모델명 & 상품명 한 줄로 배치 */}
-        <NameRowContainer>
-          <div>
-            <Label htmlFor="modelName">모델명 *</Label>
-            <Input
-              type="text"
-              name="modelName"
-              id="modelName"
-              placeholder="모델명"
-              value={product.modelName}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div>
-            <Label htmlFor="productName">상품명 *</Label>
-            <Input
-              type="text"
-              name="productName"
-              id="productName"
-              placeholder="상품명"
-              value={product.productName}
-              onChange={handleChange}
-            />
-          </div>
-        </NameRowContainer>
-
-        {/* 옵션, 재고 보유, 상품 카테고리 한 줄로 배치 */}
-        <RowContainer>
-          <div>
-            <Label htmlFor="option">옵션 *</Label>
-            <Input
-              type="text"
-              name="option"
-              id="option"
-              placeholder="구분자"
-              value={product.option}
-              onChange={handleChange}
-            />
-          </div>
-
-
-          <div>
-            <Label htmlFor="category">상품 카테고리 *</Label>
-            <Select
-              name="category"
-              id="category"
-              value={product.category}
-              onChange={handleChange}
-            >
-              <option value="">대분류명</option>
-              <option value="category1">카테고리1</option>
-              <option value="category2">카테고리2</option>
-            </Select>
-          </div>
-        </RowContainer>
-
-          {/* Right Column: 가격 */}
-          <RightColumn>
+        <C.NameRowContainer>
             <div>
-              <Label htmlFor="businessPrice">Business *</Label>
-              <Input
-                type="number"
-                name="businessPrice"
-                id="businessPrice"
-                placeholder="숫자만 입력해 주세요."
-                value={product.businessPrice}
+              <C.Label htmlFor="productModel">모델명 *</C.Label>
+              <C.Input
+                type="text"
+                name="productModel"
+                id="modelName"
+                value={product.productModel}
                 onChange={handleChange}
               />
             </div>
-
             <div>
-              <Label htmlFor="bestPrice">Best *</Label>
-              <Input
-                type="number"
-                name="bestPrice"
-                id="bestPrice"
-                placeholder="숫자만 입력해 주세요."
-                value={product.bestPrice}
+              <C.Label htmlFor="productName">상품명 *</C.Label>
+              <C.Input
+                type="text"
+                name="productName"
+                id="productName"
+                placeholder="상품명"
+                value={product.productName}
                 onChange={handleChange}
               />
             </div>
-
+          </C.NameRowContainer>
+          <C.RowContainer>
             <div>
-              <Label htmlFor="dealerPrice">Dealer *</Label>
-              <Input
-                type="number"
-                name="dealerPrice"
-                id="dealerPrice"
-                placeholder="숫자만 입력해 주세요."
-                value={product.dealerPrice}
+              <C.Label htmlFor="options">상품 카테고리 *</C.Label>
+              <C.Input
+                type="text"
+                name="options"
+                id="options"
+                value={product.options.join(', ')}
                 onChange={handleChange}
               />
             </div>
-
             <div>
-              <Label htmlFor="customerPrice">Customer *</Label>
-              <Input
-                type="number"
-                name="customerPrice"
-                id="customerPrice"
-                placeholder="숫자만 입력해 주세요."
-                value={product.customerPrice}
+              <C.Label htmlFor="category">옵션 *</C.Label>
+              <C.Select
+                name="category"
+                id="category"
+                value={product.category}
+                onChange={handleChange}
+              >
+                <option value="">옵션 선택</option>
+                <option value="Noise Cancelling">Noise Cancelling</option>
+                <option value="Bluetooth 5.0">Bluetooth 5.0</option>
+                <option value="Built-in Microphone">Built-in Microphone</option>
+              </C.Select>
+            </div>
+            </C.RowContainer>
+            <C.RightColumn>
+              <div>
+                <C.Label htmlFor="businessPrice">Business *</C.Label>
+                <C.Input
+                  type="number"
+                  name="businessPrice"
+                  id="businessPrice"
+                  placeholder="숫자만 입력해 주세요."
+                  value={product.businessPrice}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                <C.Label htmlFor="bestPrice">Best *</C.Label>
+                <C.Input
+                  type="number"
+                  name="bestPrice"
+                  id="bestPrice"
+                  placeholder="숫자만 입력해 주세요."
+                  value={product.bestPrice}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                <C.Label htmlFor="dealerPrice">Dealer *</C.Label>
+                <C.Input
+                  type="number"
+                  name="dealerPrice"
+                  id="dealerPrice"
+                  placeholder="숫자만 입력해 주세요."
+                  value={product.dealerPrice}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                <C.Label htmlFor="customerPrice">Customer *</C.Label>
+                <C.Input
+                  type="number"
+                  name="customerPrice"
+                  id="customerPrice"
+                  placeholder="숫자만 입력해 주세요."
+                  value={product.customerPrice}
+                  onChange={handleChange}
+                />
+              </div>
+            </C.RightColumn>
+            <C.RightColumn>
+            <div>
+              <C.Label htmlFor="photo">Photo *</C.Label> {/* 이미지 순서 변경 로직 필요 */}
+              <C.FileInput
+                type="file"
+                name="photo"
+                id="photo"
                 onChange={handleChange}
               />
             </div>
-          </RightColumn>
-
-          {/* Toast UI Editor */}
-          <div>
-            <div style={{marginBottom: '10px'}}><Label  htmlFor="content">상품 설명 *</Label></div>
-            <Editor
-              ref={editorRef}
-              initialValue="상품 설명을 입력하세요."
-              previewStyle="vertical"
-              height="500px"
-              initialEditType="wysiwyg"
-              useCommandShortcut={false}
-              toolbarItems={[
-                ['heading', 'bold', 'italic', 'strike'],
-                ['hr', 'quote'],
-                ['ul', 'ol', 'task', 'indent', 'outdent'],
-                ['table', 'link', 'image']
-              ]}
-              onChange={handleEditorChange}
+            </C.RightColumn>
+            {/* Toast UI Editor */}
+            <div>
+              <div style={{marginBottom: '10px'}}><C.Label  htmlFor="content">상품 설명 *</C.Label></div>
+              <Editor
+                ref={editorRef}
+                initialValue="상품 설명을 입력하세요."
+                previewStyle="vertical"
+                height="500px"
+                initialEditType="wysiwyg"
+                useCommandShortcut={true}
+                hooks={{
+                  addImageBlobHook: addImageBlobHook
+                }}
+                toolbarItems={[
+                  ['heading', 'bold', 'italic', 'strike'],
+                  ['hr', 'quote'],
+                  ['ul', 'ol', 'task', 'indent', 'outdent'],
+                  ['table', 'link', 'image']
+                ]}
+                onChange={handleEditorChange}
             />
-          </div>
 
-        </div>
-      </FormWrapper>
-      <N.Section style={{margin: '0'}}>
-      <D.BtWrap>
-            <D.BtLink as={Link} to="">
-              취소
-            </D.BtLink>
-            <D.BtLink as={Link} to="">
-              등록
-            </D.BtLink>
-          </D.BtWrap>
-          </N.Section>
-    </MainWrapper>
-  );
+            </div>
+
+            </div>
+            </C.FormWrapper>
+            <N.Section style={{margin: '0'}}>
+            <D.BtWrap>
+              <D.BtLink as={Link} to="">
+                취소
+              </D.BtLink>
+              <D.BtLink onClick={handleCreateProduct}>
+                등록
+              </D.BtLink>
+            </D.BtWrap>
+            </N.Section>
+            </C.MainWrapper>
+            );
 };
 
 export default ProductCreate;
+
---
+++ src/components/Admin/Product/ProductCreateStyle.jsx
@@ -0,0 +1,94 @@
+import styled from 'styled-components';
+
+
+export const MainWrapper = styled.div`
+  width: 78%; 
+  margin-left: 22%;
+  padding: 20px;
+`;
+
+
+export const FormWrapper = styled.div`
+  width: 100%;
+  border: 1px solid #e5e5e5;
+  border-radius: 8px;
+`;
+
+export const SectionTitleWrapper = styled.div`
+  background-color: #f2f2f2; 
+  padding: 1%;
+`;
+
+export const SectionTitle = styled.h3`
+  font-size: 2.3rem;
+  font-weight: bold;
+`;
+
+
+export const Label = styled.label`
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10%;
+  margin-top: 3%;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: 1rem;
+  box-sizing: border-box;
+`;
+
+export const Select = styled.select`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10%;
+  border: 1px solid #ddd;
+  margin-top: 3%;
+  border-radius: 5px;
+  font-size: 1rem;
+  box-sizing: border-box;
+`;
+
+export const FileInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 10%;
+  margin-top: 3%;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  font-size: 1rem;
+  box-sizing: border-box;
+`;
+
+
+export const RowContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); 
+  grid-column-gap: 20px;
+  margin-bottom: 10px;
+`;
+
+
+export const NameRowContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); 
+  grid-column-gap: 20px;
+  margin-bottom: 10px;
+`;
+
+
+export const RightColumn = styled.div`
+  flex-direction: row;
+  display: flex;
+  justify-content: space-between;
+`;
+
+
+export const SplitContainer = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr; 
+  grid-gap: 20px;
+`;
